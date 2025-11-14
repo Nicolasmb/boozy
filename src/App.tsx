fc790251b5d4f673e64adb6a24ecb51d18faad7e
Nicolasmb <nicolasbarada@gmail.com>
2025-11-14 15:12:45 -0300

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import './App.css'
+
+function App() {
+
+	return (
+		<>
+			<div className='desktop'>
+				<div className='topbar'>
+					<div className='container'>
+						<div className='bar'>
+							<a href="/" aria-current="page" className="boozy w-inline-block w--current">
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className='viewport'>
+					<div className='main-content'>
+						<div className='row'>
+							<div className='column'></div>
+							<div className='column'></div>
+						</div>
+					</div>
+				</div>
+				<div className='bottom-bar'>
+					<div className='container'>
+						<div className='footer'>
+							<div className='footer-left-links'>
+								<a href="" draggable="false" role="menuitem">
+									<span>BOOZY</span>
+								</a>
+								<a href="" draggable="false" role="menuitem">
+									<span>EL BLEN MAS SEXY</span>
+								</a>
+								<a href="" draggable="false" role="menuitem">
+									<span>PRESENTACION </span>
+								</a>
+								<a href="" draggable="false" role="menuitem">
+									<span>FUSION</span>
+								</a>
+								<a href="" draggable="false" role="menuitem">
+									<span>DIVERTIDO</span>
+								</a>
+								<a href="" draggable="false" role="menuitem">
+									<span>SE PARTE </span>
+								</a>
+								<a href="" draggable="false" role="menuitem">
+									<span>PREGUNTAS</span>
+								</a>
+								<a href="" draggable="false" role="menuitem">
+									<span>ES AHORA</span>
+								</a>
+								<a href="" draggable="false" role="menuitem">
+									<span>CONTACTO</span>
+								</a>
+							</div>
+							<div className='footer-right-links'></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default App
